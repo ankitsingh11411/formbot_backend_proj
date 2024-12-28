7f6ad3f42807c88428282100eee1879c39be9c3f
Ankit Singh <aksingh11411@gmail.com>
2024-12-28 16:06:57 +0530

    schema import updates

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ const folderRoutes = require('./routes/folderRoutes');
 const formRoutes = require('./routes/formRoutes');
 const responseRoutes = require('./routes/responseRoutes');
 
-require('dotenv').config();
-
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,10 +22,7 @@ app.get('/', (req, res) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('Database connection error:', err));
 
@@ -34,4 +30,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/folders', folderRoutes);
 app.use('/api/forms', formRoutes);
 app.use('/api/responses', responseRoutes);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
